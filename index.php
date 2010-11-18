fa7d80f2617e71c19fee78f3467b22018ce1d778
Néstor Arocha Rodríguez <nesaro@gmail.com>
2010-11-18 08:26:03 +0100

    Comment translation

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
     $binary = "/home/nesaro/proyectos/translator/git/bin/clnyiwrapper.sh";
     echo exec($binary." -t simple-adder -e {\\\"input\\\":\\\"1+1\\\"}");
 
-//Si se pasa el index sin nada,
-//listar los programas
-//si se pasa con f, mostrar el form de la funcion
-//si se pasa con f + parametros, devolver el resultado
+    //if $_POST array if empty:
+        //Call command to get function index
+    //if $_POST only have function key:
+        //call command to get the function form
+    //if $_POST have function and any other key
+        //call command to get execution result
+
 ?>
