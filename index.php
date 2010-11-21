f97702317ddb174485990dae86e887664d153c25
Néstor Arocha Rodríguez <nesaro@gmail.com>
2010-11-21 18:50:04 +0100

    Index page when $_POST is empty

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
     $binary = "/home/nesaro/proyectos/translator/git/bin/clnyiwrapper.sh";
-    echo exec($binary." -t simple-adder -e {\\\"input\\\":\\\"1+1\\\"}");
+    #echo exec($binary." -t simple-adder -e {\\\"input\\\":\\\"1+1\\\"}");
 
     //if $_POST array if empty:
-        //Call command to get function index
+    //Call command to get function index
+    if (count($_POST) == 0)
+    {
+        echo exec($binary." -c webindex -p");
+    }
     //if $_POST only have function key:
         //call command to get the function form
     //if $_POST have function and any other key
