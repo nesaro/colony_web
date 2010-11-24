20174ec070f1f20730572636d969cda4c1bfd2cc
Néstor Arocha Rodríguez <nesaro@gmail.com>
2010-11-24 22:27:41 +0100

    translator application form

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
 
     //if $_POST array if empty:
     //Call command to get function index
-    if (count($_POST) == 0)
+    if (count($_POST) == 0 && count($_GET) == 0)
     {
         echo exec($binary." -c webindex -p");
+    }
+    if (count($_POST) == 0)
+    {
+        if (isset($_GET["mode"]) && isset($_GET["app"]) && ($_GET["mode"] == "translate"))
+        {
+            echo exec($binary." -t webdisplaypage -e {\\\"input\\\":\\\"".$_GET["app"]."\\\"} -o {\\\"output\\\":\\\"stdout\\\"}");
+        }
+
     }
     //if $_POST only have function key:
         //call command to get the function form
