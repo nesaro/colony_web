7067e4c16b349e0ae0765fc95a1acd7272e715c0
Néstor Arocha Rodríguez <nesaro@gmail.com>
2010-11-30 20:07:32 +0100

    shows appsession list on empty arglist

--- result.php
+++ result.php
@@ -1,8 +1,45 @@
 <?php
+    include('session.php');
+    $uid = checkSession();
+    if (!$uid)
+    {
+        header('Location:login.php');
+        return;
+    }
+
+?>
+    <a href="do_logout.php">Logout</a>
+<?php
 
 //Results:
 //If not $_GET && not $_POST,
 //show a list of results for current user.
-//If no user (no session), go to login or show error
+
+
+    if (count($_GET) == 0)
+    {
+        $query = "SELECT * FROM apprequest WHERE iduser='".$uid."';";
+        $result = mysql_query($query) or die(mysql_error());
+        //ask db for every
+        //echo "<form method=\"GET\">";
+        while($info = mysql_fetch_array( $result ))
+        {
+            echo '<a href="result.php?requestsession='.$info['appsession'].'">'.$info['appsession'].'</a><br />';
+        }
+        //echo "</form>";
+            
+        return;
+    }
+    elseif ((count($_GET) > 0) && isset($_GET['requestsession']))
+    {
+        $query = "SELECT * FROM apprequest WHERE requestsession='".$_GET['requestsession']."';";
+        $result = mysql_query($query) or die(mysql_error());
+        $info = mysql_fetch_array( $result );
+    }
+    else
+    {
+        echo 'Unknown mode';
+        return
+    } 
 
 ?>
