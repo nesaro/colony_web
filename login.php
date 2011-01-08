d665c6eb3b8857e4f313ca4e149292acbd3dfbe3
Néstor Arocha Rodríguez <nesaro@gmail.com>
2011-01-08 20:16:48 +0100

    form password field is password type

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <body>
 <form action="do_login.php" method="POST">
 username: <input type="text" name="user" /><br />
-password: <input type="text" name="pass" /><br />
+password: <input type="password" name="pass" /><br />
 <input type="submit" value="send" />
 </form>
 </body>
