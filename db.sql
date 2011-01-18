0b42e2846131bf1a5e660d10de80e6db6c469b9e
Néstor Arocha Rodríguez <nesaro@gmail.com>
2011-01-18 11:44:04 +0100

    userlevel table

--- db.sql
+++ db.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS users
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(12) UNIQUE NOT NULL,
-    password VARCHAR(12) NOT NULL
+    password VARCHAR(12) NOT NULL,
+    userlevel INT NOT NULL,
+    FOREIGN KEY (userlevel) REFERENCES userlevel(name)
 );
 
 CREATE TABLE IF NOT EXISTS websession
@@ -19,3 +21,14 @@ create TABLE IF NOT EXISTS apprequest
     iduser INT NOT NULL,
     tmppath VARCHAR(255)
 );
+
+create TABLE IF NOT EXISTS userlevel
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR("12"),
+    description TEXT
+);
+
+INSERT INTO userlevel ('name','description') VALUES ('Basic','restricted library, ads, no personal space, no fee');
+INSERT INTO userlevel ('name','description') VALUES ('Standard','less restricted library, no ads, 10MB personal space, small fee');
+INSERT INTO userlevel ('name','description') VALUES ('Premium','full access library, no ads, 1GB personal space, big fee');
